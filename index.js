1272efd09465649a4ac2cbaa03635f4a53f4a055
aiegoo <eozz21@gmail.com>
2021-11-14 10:59:37 +0900

    update lesson 7 index.js

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const canvas = d3.select('.canvas');
 const svg = canvas.append('svg')
 svg.attr('width', 1200).attr('height', 700);
-svg.append('rect')
-     .attr('width', 600)
-     .attr('height', 200)
-     .attr('background-color', '#f0f');
-svg.append('circle')
-     .attr('radius', 200)
-     .attr('fill', 'blue');
+const group = svg.append('g')
+     .attr('transform', 'translate(-10, 90)');
 
-svg.append('line')
-     .attr();
+group.append('rect')
+     .attr('width', 200)
+     .attr('height', 100)
+     .attr('fill', '#f0f')
+     .attr('x', 20)
+     .attr('y', 20);
+group.append('circle')
+     .attr('r', 60)
+     .attr('fill', 'blue')
+     .attr('cx', 400)
+     .attr('cy', 80);
+
+group.append('line')
+     .attr('x1', 570)
+     .attr('x2', 600)
+     .attr('y1', 20)
+     .attr('y2', 120)
+     .attr('stroke', 'red')
+     .attr('stroke-width', 5);
+
+svg.append('text')
+     .attr('x', 20)
+     .attr('y', 100)
+     .attr('fill', 'grey')
+     .text('hello world');
 
 console.log(svg);
