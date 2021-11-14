a4639427c63dcf598201d3ed0077a3d081e49f37
aiegoo <eozz21@gmail.com>
2021-11-14 11:08:16 +0900

    update index.js with data binding initial setup

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-const canvas = d3.select('.canvas');
-const svg = canvas.append('svg')
-svg.attr('width', 1200).attr('height', 700);
-const group = svg.append('g')
-     .attr('transform', 'translate(-10, 90)');
+const data = [
+     {width: 200, height: 100, fill:'purple'}
+];
 
-group.append('rect')
+const svg = d3.select('svg');
+
+svg.select('rect')
+     .data(data)
      .attr('width', 200)
      .attr('height', 100)
-     .attr('fill', '#f0f')
-     .attr('x', 20)
-     .attr('y', 20);
-group.append('circle')
-     .attr('r', 60)
-     .attr('fill', 'blue')
-     .attr('cx', 400)
-     .attr('cy', 80);
-
-group.append('line')
-     .attr('x1', 570)
-     .attr('x2', 600)
-     .attr('y1', 20)
-     .attr('y2', 120)
-     .attr('stroke', 'red')
-     .attr('stroke-width', 5);
-
-svg.append('text')
-     .attr('x', 20)
-     .attr('y', 100)
-     .attr('fill', 'grey')
-     .text('hello world');
-
-console.log(svg);
+     .attr('fill', 'pink');
