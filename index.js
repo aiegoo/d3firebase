5f5d04aef703901ff7a7fc04dafb2abf728ad462
aiegoo <eozz21@gmail.com>
2021-11-22 14:29:29 +0900

    lesson 20 pratcice

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 // select the svg conatiner first
 const svg = d3.select('svg');
 
-d3.json('planets.json').then(data => {
+const data = d3.json('planets.json').then(data => {
+// join the data to circs
+const circs = svg.selectAll('circle')
+.data(data);
 
-  // join the data to circs
-  const circs = svg.selectAll('circle')
-    .data(data);
+circs.attr('cy', 200)
+.attr('cx', d => d.distance)
+.attr('r', d => d.radius)
+.attr('fill', d => d.fill);
 
-  // add attrs to circs already in the DOM
-  circs.attr('cy', 200)
-    .attr('cx', d => d.distance)
-    .attr('r', d => d.radius)
-    .attr('fill', d => d.fill);
-
-  // append the enter selection to the DOM
-  circs.enter()
-    .append('circle')
-      .attr('cy', 200)
-      .attr('cx', d => d.distance)
-      .attr('r', d => d.radius)
-      .attr('fill', d => d.fill);
-
+// append the enter selection to the dome
+circs.enter()
+.append('circle')
+.attr('cy', 200)
+.attr('cx', d => d.distance)
+.attr('r', d => d.radius)
+.attr('fill', d => d.fill);
+console.log(circs)
 });
-
