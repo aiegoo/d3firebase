63b194ac218773d276aa668a2443b9d7f0e53e51
aiegoo <eozz21@gmail.com>
2021-11-22 16:28:25 +0900

    add lesson 22 with data joining and linear scale

--- index.js
+++ index.js
@@ -2,30 +2,27 @@
 const svg = d3.select('svg');
 
 d3.json('menu.json').then(data => {
+     console.log(data)
+     const y = d3.scaleLinear()
+          .domain([0, 1000])
+          .range([0, 500]);
+console.log(y(400))
+console.log(y(1))
+console.log(y(950))
+const rects = svg.selectAll('rect')
+     .data(data)
+rects.attr('width', 50)
+     .attr('height', d => y(d.orders))
+     .attr('fill', 'orange')
+     .attr('x', (d, i) => i * 70)
 
-  const y = d3.scaleLinear()
-    .domain([0, 1000])
-    .range([0, 500]);
+console.log(rects)
 
-  //console.log(y(600))
-
-  // join the data to circs
-  const rects = svg.selectAll('rect')
-    .data(data);
-
-  // add attrs to circs already in the DOM
-  rects.attr('width', 50)
-    .attr("height", d => y(d.orders))
-    .attr('fill', 'orange')
-    .attr('x', (d, i) => i * 70)
-
-  // append the enter selection to the DOM
-  rects.enter()
-    .append('rect')
-      .attr('width', 50)
-      .attr("height", d => y(d.orders))
-      .attr('fill', 'orange')
-      .attr('x', (d, i) => i * 70)
-
-});
+rects.enter()
+     .append('rect')
+          .attr('width', 50)
+          .attr('height', d => y(d.orders))
+          .attr('fill', 'hotPink')
+          .attr('x', (d, i) => i * 70)
+})
 
