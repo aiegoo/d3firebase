6e9d34c414dc643d7ec9a6bd7194868e1469bc52
aiegoo <eozz21@gmail.com>
2021-11-22 10:44:03 +0900

    index.js for arrow function and how it's different from regular f

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const data = [
-     {width: 200, height: 100, fill:'purple'}
+     {width: 200, height: 400, fill:'pink'}
 ];
 
 const svg = d3.select('svg');
 
 const rect = svg.select('rect')
      .data(data)
-     .attr('width', function(d, i , n){ 
+     .attr('width', (d, i , n) => { 
+          console.log(this)
           console.log(i)
           console.log(n)
           return d.width  
      })
-     .attr('height', function(d){ return d.height })
+     .attr('height', function(d){ 
+          console.log(this)
+          return d.height })
      .attr('fill', function(d){ return d.fill });
 console.log(rect);
